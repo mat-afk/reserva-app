0e0b653fc012d5198b6e22e28f104e847af026d3
Mateus <mateusc.ramos@outlook.com>
2024-07-23 21:54:53 -0300

    refactor: update file path and parent directory

--- reserva_app/repository/repository.py
+++ reserva_app/repository/repository.py
@@ -4,29 +4,37 @@ from pathlib import Path
 
 class Repository:
 
-    DATABASE_PATH = Path(__file__).parents[2] / "database"
+    DATABASE_PATH = Path(__file__).resolve().parents[2] / "database"
     ID_OFFSET = 1
 
     def __init__(self, source_path: str):
         self.file_path = self.DATABASE_PATH / source_path
 
+        if not self.file_path.parent.exists():
+            self.file_path.parent.mkdir(parents=True, exist_ok=True)
+
     def list(self) -> list[Model]:
+        if self.file_path.exists():
+            return []
+        
         with open(self.file_path) as file:
             return [self.convert_to_model(row) for row in file]
 
     def save(self, model: Model) -> int:
+        model.id = self.new_id()
+        row = model.to_row()
+        
         with open(self.file_path, "a") as file:
-            model.id = self.new_id()
-
-            row = model.to_row()
             file.write(row)
 
-            return model.id
+        return model.id
 
     def find_by_id(self, id: int) -> Model:
         for model in self.list():
             if model.id == id:
                 return model
+            
+        return None
 
     def convert_to_model(self, row: str) -> Model:
         return None
@@ -35,7 +43,8 @@ class Repository:
         return text.strip().lower() in ("true", "1")
     
     def new_id(self) -> int:
-        return self.ID_OFFSET if not self.list() else self.list()[-1].id + 1
+        models = self.list()
+        return self.ID_OFFSET if not models else models[-1].id + 1
 
 
 class SalaRepository(Repository):
@@ -54,13 +63,7 @@ class SalaRepository(Repository):
         id = int(id)
         ativa = self.str_to_bool(ativa)
         
-        return Sala( 
-            capacidade,
-            tipo, 
-            descricao,
-            id=id,
-            ativa=ativa,
-        )
+        return Sala(capacidade, tipo, descricao, id=id, ativa=ativa)
 
 
 salaRepository = SalaRepository()
